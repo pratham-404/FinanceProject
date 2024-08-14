904b6878e4aca6072ff98c7988ea2b72c96a9bb6
unknown <waskarsakshi52@gmail.com>
2024-08-14 10:18:59 +0530

    deactivate users functionality

--- src/dao/DatabaseConnection.java
+++ src/dao/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 	public static Connection getConnection() {
         String url = "jdbc:mysql://localhost:3306/finance_app"; // Update with your database URL
         String user = "root"; // Update with your database username
-        String password = "root"; // Update with your database password
+        String password = "Naruto@4523"; // Update with your database password
         //String password = "root"; // Update with your database password
 
         try {
